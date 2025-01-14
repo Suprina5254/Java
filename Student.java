a6184a5b75c7484ce6f30127e1d52ae16f7c97ae
Suprina5254 <np03cs4a230415@heraldcollege.edu.np>
2025-01-14 08:58:05 +0545

    implemented getter

--- Student.java
+++ Student.java
@@ -1,5 +1,22 @@
 package Workshop8;
 
 public class Student {
+	    private String name;
+	    private int age;
 
-}
+	    
+	    public Student(String name, int age) {
+	        this.name = name;
+	        this.age = age;
+	    }
+
+		public String getName (){
+			return name;
+		}
+
+		public int getAge (){
+			return age;
+		}
+	}
+
+
